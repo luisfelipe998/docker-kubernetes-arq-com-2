a74ea5cec8ad707d80fedb49ed0c4f0f6162190c
Luis <luis.borsoi@sap.com>
2023-11-22 20:51:46 -0300

    adjust scripts

--- k8s_install.sh
+++ k8s_install.sh
@@ -1,13 +1,7 @@
-kubectl delete deployment app
-kubectl delete service app-udp
-kubectl delete deployment mongodb
-kubectl delete service mongodb
-kubectl delete pvc db
-kubectl delete networkpolicy network
+sh ./k8s_uninstall.sh
 
-kubectl apply -f ./k8s/network-networkpolicy.yaml
 kubectl apply -f ./k8s/mongodb-deployment.yaml
 kubectl apply -f ./k8s/mongodb-service.yaml
 kubectl apply -f ./k8s/db-persistentvolumeclaim.yaml
 kubectl apply -f ./k8s/app-deployment.yaml
-kubectl apply -f ./k8s/app-udp-service.yaml
+kubectl apply -f ./k8s/app-tcp-service.yaml
---
+++ k8s_uninstall.sh
@@ -0,0 +1,5 @@
+kubectl delete deployment app
+kubectl delete service app-tcp
+kubectl delete deployment mongodb
+kubectl delete service mongodb
+kubectl delete pvc db
